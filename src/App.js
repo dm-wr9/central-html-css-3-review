45aff4c435f885fc957a7c13b86b8ff6aa632b14
Cole Finlayson <cole.fin8@gmail.com>
2021-02-05 08:58:29 -0700

    finished review

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hamburgerClass: "",
+    };
+  }
+
+  handleClassChange = () => {
+    this.setState({
+      hamburgerClass: this.state.hamburgerClass === "" ? "show" : "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="keyframe box">Keyframe</div>
+        <div className="media box">
+          <button onClick={this.handleClassChange}>Hamburger</button>
+          <ul className={`list ${this.state.hamburgerClass}`}>
+            <li>One</li>
+            <li>2</li>
+            <li>III</li>
+            <li>Four</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
